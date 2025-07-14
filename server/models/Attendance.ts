d3d32906cf44fedf04819a1d19eac61f3828a64e
Chirag8405 <chiragpoornamath@gmail.com>
2025-07-14 14:27:38 +0530

    Update Attendance.ts

--- server/models/Attendance.ts
+++ server/models/Attendance.ts
@@ -1,4 +1,4 @@
-import { getDatabase } from "../database/connection";
+import { getDatabase } from "../database/connection.js";
 import { v4 as uuidv4 } from "uuid";
 
 export type AttendanceStatus = "present" | "absent" | "cancelled";
